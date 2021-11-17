58af9713548484d8dcf00ec737af808437cf5063
Kevin <kevin.jakuszkowiak@gmail.com>
2021-11-16 20:56:35 -0600

    added product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+    validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
