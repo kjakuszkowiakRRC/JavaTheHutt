e5b3611755a2c6178f40683360abb01916471f9b
Kevin <kevin.jakuszkowiak@gmail.com>
2021-11-17 18:08:49 -0600

    added provincial taxes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Variety.delete_all
-# Product.delete_all
-ProvinceTax.delete_all
-Province.delete_all
-Tax.delete_all
+Variety.delete_all
+Product.delete_all
+# ProvinceTax.delete_all
+# Province.delete_all
+# Tax.delete_all
 
 require "csv"
 
@@ -36,9 +36,20 @@ require "csv"
 
 # end
 
-# for i in 0..5 do
-#     Variety.create(name: Faker::Coffee.unique.variety)
+# province_taxes_csv_file_location = Rails.root.join('db/Province_Taxes.csv')
+# csv_data = File.read(province_taxes_csv_file_location)
+# province_taxes = CSV.parse(csv_data, headers: true)
+
+# province_taxes.each do |province_tax|
+
+#     ProvinceTax.create(province_id: province_tax[0],
+#                        tax_id: province_tax[1])
+
 # end
+
+for i in 0..5 do
+    Variety.create(name: Faker::Coffee.unique.variety)
+end
 
 # for i in 0..ran(10) do
 #     Product.create(name: "TESTER", price: 3.50)
