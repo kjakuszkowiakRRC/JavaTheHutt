97ace7df2e94c1212135229cfc03cd0649edd8f6
Kevin <kevin.jakuszkowiak@gmail.com>
2021-11-20 15:37:11 -0600

    revert user/payments

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  belongs_to :province
-  has_many :payments
-
-  validates :username, presence: true
-  validates :password, presence: true
-  validates :street_address, presence: true
-  validates :province_id, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_211310) do
+ActiveRecord::Schema.define(version: 2021_11_20_213523) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,15 +36,6 @@ ActiveRecord::Schema.define(version: 2021_11_20_211310) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "payments", force: :cascade do |t|
-    t.string "credit_card_number"
-    t.decimal "amount"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -81,25 +72,13 @@ ActiveRecord::Schema.define(version: 2021_11_20_211310) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "street_address"
-    t.integer "province_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["province_id"], name: "index_users_on_province_id"
-  end
-
   create_table "varieties", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "payments", "users"
   add_foreign_key "products", "varieties"
   add_foreign_key "province_taxes", "provinces"
   add_foreign_key "province_taxes", "taxes"
-  add_foreign_key "users", "provinces"
 end
